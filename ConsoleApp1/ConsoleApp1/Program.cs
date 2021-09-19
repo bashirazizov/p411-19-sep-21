56c6a9e05d168da02405e0c5b202295758766b86
bashirazizov <bashir.azizov@code.edu.az>
2021-09-19 20:25:47 +0400

    Init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1&2
+            //Console.Write("Eded daxil et: ");
+            //int max = Convert.ToInt32(Console.ReadLine());
+            //int result = 0;
+
+            //for (int i = 1; i < max; i += 2)
+            //{
+            //    result += i;
+            //}
+
+            //Console.WriteLine(result);
+
+            //Console.Write("Ededi daxil edin: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Quvveti daxil edin: ");
+            //int pow = Convert.ToInt32(Console.ReadLine());
+
+            //int result = 1;
+
+            //while (pow>=1)
+            //{
+            //    result *= num;
+            //    pow--;
+
+            //}
+
+            //Console.WriteLine(result);
+            #endregion
+            #region try catch
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(num);
+
+
+            //string[] students = { "samir", "mamed", "sahin", "ali"};
+
+            //Console.Write("necenci telebe lazimdir? ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+
+            //try
+            //{
+            //    Console.WriteLine(students[num]);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("problem");
+            //}
+
+
+            //string[] students = { "samir", "mamed", "sahin", "ali"};
+            //int leftAttemps = 3;
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.Write("necenci telebe lazimdir? ");
+            //        int num = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine(students[num]);
+            //        break;
+            //    }
+            //    catch (Exception)
+            //    {
+            //        leftAttemps--;
+            //        if (leftAttemps==0)
+            //        {
+            //            break;
+            //        }
+            //        Console.WriteLine("Problem var."+ leftAttemps +"sansiniz qalib");
+            //    }
+            ////}
+
+            //int num1;
+            //int num2;
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.Write("1ci ededi daxil edin ");
+            //        num1 = Convert.ToInt32(Console.ReadLine());
+            //        break;
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Console.WriteLine("Problem");
+            //    }
+            //}
+
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.Write("2ci ededi daxil edin ");
+            //        num2 = Convert.ToInt32(Console.ReadLine());
+            //        break;
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Console.WriteLine("Problem");
+            //    }
+            //}
+
+            //Console.WriteLine(num1 + num2);
+            #endregion
+            #region methods
+            //int[] numbers = { 10, 12, 14 };
+            //int sum = 0;
+            //int average;
+            //foreach (int num in numbers)
+            //{
+            //    sum += num;
+            //}
+
+            //average = sum / numbers.Length;
+
+
+            //int[] numbers = { 10, 12, 14 };
+            //int[] numbers2 = { 14212, 141242, 52321 };
+            //int[] numbers3 = { 14212, 41,123,123,123,123,12,3,123123 };
+
+            //Console.WriteLine(Average(numbers));
+            //Console.WriteLine(Average(numbers2));
+            //Console.WriteLine(Average(numbers3));
+
+
+            //PrintInfo("Mamed", "Aliyev");
+
+            //Console.WriteLine( Sum(1,2) );
+            //ErrorVar();
+
+            //string[] names = { "Mamed", "Nicat", "Kamran" };
+
+            //Console.WriteLine(CombineStrings(names));
+
+            //Console.WriteLine(CombineStrings("GulMamed", "AyNicat", "BagdaKamran"));
+            //int avv = Average(1, 312, 4123, 123);
+
+            //PrintInfo("memed", "aliyev", true);
+            #endregion
+
+        }
+
+        static string CombineStrings(params string[] strings)
+        {
+            string result = "";
+
+            foreach (string item in strings)
+            {
+                result += item + ", ";
+            }
+
+            return result;
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x+y;
+        }
+
+        static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void PrintInfo(string name, string surname, bool isMarried = false)
+        {
+            string result = name + " " + surname;
+            if (isMarried)
+            {
+                result += " evlidir";
+            }
+            else
+            {
+                result += " subaydir";
+            }
+
+            Console.WriteLine(result);
+        }
+
+
+
+        static void ErrorVar()
+        {
+            Console.WriteLine("Daxil etdiyiniz melumatlar yanlisdir.");
+        }
+
+
+
+        static int Average(params int[] arr)
+        {
+            int sum = 0;
+            int average;
+
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+
+            average = sum / arr.Length;
+
+            return average;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # p411-19-sep-21
+
+### Tapşırıqlar
+Hamısı bir console app-in içində fərqli regionlarda yazılmalıdır. Sonda kodu github-da yaratdığınız public repoya push edib linkləri mailimə göndərin. Deadline: 24-sen-21 18:00.
+
+1.Tələbənin balı daxil edilir. Əgər bal 100-91 aralığındadırsa A, 90-81 aralığındadırsa B, 80-71 aralığındadırsa C, 70-61 aralığındadırsa D, 60-51 aralığındadırsa E, 50 və 50dən aşağıdırsa kəsr print edən app yazın. Bal 0-100 arasında olmazsa error print olunmalıdır. Proqram valid nəticə çıxarmadıqca dayanmamalıdır. ( while və try catch istifadə edin )
+
+2.Daxil edilmiş ədədin tam bolənlərini print edən app yazın.
+
+3.Daxil edilmiş ədədin neçə mərtəbəli olduğunu print edən app yazın.
+
+4.Daxil edilmiş ədədin rəqəmlərinin cəmini hesablayıb print edən app yazın.
+
+5.Qəbul etdiyi ədədin sadə olub olmadığını yoxlayan method yazın. Metod əgər göndərilən ədəd sadədirsə true deyilsə false qaytarmalıdır.
+
+6.Qəbul etdiyi ədədin faktorialını hesablayıb qaytaran method yazın.
+
+7.Rəqəmlər array-ı verilir. Array-dakı ən böyük və ən kiçik ədədlərin cəmini tapan app yazın.
+
+8.1 ilə 4 arasında ədəd daxil edilir. Bu ədədə əsasən fəsli print edən app yazın. (1-yaz, 2-yay, 3-payız, 4-qış)
+
+9.Qəbul etdiyi string array-ından ən uzun string-i qaytaran method yazın.
+
+10.1-10 arası ədədlərin (1 və 10 daxil olmaqla) vurma cədvəlini print edən app yazın. (loop istifadə edin)
+
+## Əlavə olaraq videoları yenidən izləyin, keçdiyimiz mövzular barədə biliklərinizi bərkidin.
